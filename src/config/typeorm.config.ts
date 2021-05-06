83ac496f7c982231b97369191cde9acc63f1ca76
Jordan Marsaw <proxxist@gmail.com>
2021-05-06 15:25:06 -0700

    Update typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,7 +4,7 @@ import * as config from 'config';
 const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  url: '', 
+  url: 'postgres://mkhzwvgmqcnlct:9c7846a0f82d487d66f120f60ab899432f1bcaeab9bfa2eb43c2eeb1a7923020@ec2-107-22-83-3.compute-1.amazonaws.com:5432/defjbv1u5cou76', 
   type: 'postgres',
   autoLoadEntities: true, 
   synchronize: false 
